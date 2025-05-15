5f6f469fbb46811d5c64f302e581b14af6e90ebe
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-05-15 14:36:22 +0530

    Update items.js

--- data/items.js
+++ data/items.js
@@ -6,7 +6,7 @@ const items = [
         item_name: 'Rhodium-Plated CZ Floral Studs',
         original_price: 1045,
         current_price: 606,
-        discount_percentage: 42,
+        discount_percentage: 42, 
         return_period: 14,
         delivery_date: '10 Oct 2023',
         rating: {
