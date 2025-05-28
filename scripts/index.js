6e181b9e5143732af17255638c9c033d88b91efc
Devyani Thakre <156990204+devyaniii12@users.noreply.github.com>
2025-05-28 16:09:23 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,7 @@ function addToBag(itemId) {
   localStorage.setItem('bagItems', JSON.stringify(bagItems));
   displayBagIcon();
 }
+
 function displayBagIcon() {
   let bagItemCountElement = document.querySelector('.bag-item-count');
   if (bagItems.length > 0) {
